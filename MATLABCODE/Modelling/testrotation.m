fa294cc269462ed448804baaa651761267ddfc7f
potatisen22 <potatisen22@users.noreply.github.com>
2018-02-26 08:50:41 +0100

    Functional
    The dynamics and kinnematics calculation is functional with made up inputs

--- MATLABCODE/Modelling/testrotation.m
+++ MATLABCODE/Modelling/testrotation.m
@@ -1,29 +1,32 @@
 clc;
 close all;
 clear all;
-fi = %rotation about x2-axis
-tetha = %rotation about y1-axis
-my = %rotation about ze-axis
-dfi = %time derivative of fi
-dtetha = %time derivative of tetha
-dmy = %time derivative of my
-TETHA = [fi, tetha,my]'
-dTetha = [pfi,ptetha,pmy]' %NOT ANGULAR VELOCITY VECTOR!
+fi = pi %rotation about x2-axis (angle between Y2 and Y3 in Y-Z plane
+tetha =pi %rotation about y1-axis (angle between Z1 and Z2) in X-Z plane
+psi =pi %rotation about ze-axis (angle between Xe and X1) in Y-X plane
+dfi =0.02 %time derivative of fi
+dtetha =0.02 %time derivative of tetha
+dpsi =0.02 %time derivative of my
+TETHA = [fi, tetha,psi]'
+dTetha = [dfi,dtetha,dpsi]' %NOT ANGULAR VELOCITY VECTOR!
 internalw = [1,0,-sin(tetha);0,cos(fi),cos(tetha)*sin(fi);0,-sin(fi),cos(tetha)*sin(fi)]*dTetha;
 Rx =[1,0,0;0,cos(fi),-sin(fi);0,sin(fi),cos(fi)];
 Ry = [cos(tetha),0,sin(tetha);0,1,0;-sin(tetha),0,cos(tetha)];
-Rz =[cos(my),-sin(my),0;sin(my),cos(my),0;0,0,1];
+Rz =[cos(psi),-sin(psi),0;sin(psi),cos(psi),0;0,0,1];
 R=Rz*Ry*Rx;
 Tinv=[1,0,-sin(tetha);0,cos(fi),cos(tetha)*sin(fi);0,-sin(fi),cos(tetha)*cos(fi)];
 T = inv(Tinv);
 J =[R,zeros(3,3);zeros(3,3),T];
-Vb = 
-wb =
+Vb = [10,2,4]; %should be read from sensors?
+wb = [5,3,4]; %should be read from sensors?
 v=[Vb,wb]'
 dxi=J*v;
 %dynamics below
-m = ;%mass of the quad
-Ixx = ;
-Iyy = ;
-Izz = ;
+m = 10;%mass of the quad
+Ixx = 2;
+Iyy = 2;
+Izz = 1;
 I = [Ixx,0,0;0,Iyy,0;0,0,Izz]; %inertia matrix is diagonal with our definition of the quadrotor
+dVb = [1 1 1];%acceleration,sensor!
+dwb = [1 1 1];%sensor?
+[Ftaub]=[m.*eye(3),zeros(3);zeros(3),I]*[dVb,dwb]'+[cross(wb,(m.*Vb))';cross(wb,(I*wb'))']%(I*wb) dotproduct?, [Fb,taub] second term correct?
