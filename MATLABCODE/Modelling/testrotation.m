e0382a92ede6dce4b9feefea38e8c10add896589
potatisen22 <potatisen22@users.noreply.github.com>
2018-03-08 12:50:17 +0100

    smal changes
    small changes and comments

--- MATLABCODE/Modelling/testrotation.m
+++ MATLABCODE/Modelling/testrotation.m
@@ -18,16 +18,21 @@ R=Rz*Ry*Rx;
 Tinv=[1,0,-sin(tetha);0,cos(fi),cos(tetha)*sin(fi);0,-sin(fi),cos(tetha)*cos(fi)];
 T = inv(Tinv);
 J =[R,zeros(3,3);zeros(3,3),T];
-Vb = [0,0,0]; %should be read from sensors?
+Vb = [0,10,0]; %should be read from sensors?
 wb = [0,0,0]; %should be read from sensors?
 v=[Vb,wb]'
 dxi=J*v;
 %dynamics below
-m = 10;%mass of the quad in Kg 
-Ixx = 2;
-Iyy = 2;
-Izz = 1;
+m = 1;%mass of the quad in Kg 
+Ixx = 1; %CHECK MOMENTS OF INERTIA
+Iyy = 1;
+Izz = 2;
 I = [Ixx,0,0;0,Iyy,0;0,0,Izz]; %inertia matrix is diagonal with our definition of the quadrotor
 %% 
-Ftaub = [0,0,-m*9.82,0,0,0]' %input!
+fz = 37 %forces in z-direction
+tx = 0%torque around x-axis
+ty = 0%torque around y-axis
+tz = 0%torque around z-axis
+Ftaub = [0,0,fz,tx,ty,tz]' + [0,0,-m*9.82,0,0,0]' %input! first vector is from motors second vector is outer forces 
 dVbdwb=inv([m.*eye(3),zeros(3);zeros(3),I])*[Ftaub]-[cross(wb,(m.*Vb))';cross(wb,(I*wb'))']
+%dxi = dVbdwb + dxi ? 
