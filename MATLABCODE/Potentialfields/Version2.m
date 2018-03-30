cb96d09925a5b963720a3eb605fde4a0470ed0e5
Paula Carbó <paulacarbocubero@gmail.com>
2018-03-30 16:11:50 +0200

    Advances on matlab code for potential fields

--- MATLABCODE/Potentialfields/Version2.m
+++ MATLABCODE/Potentialfields/Version2.m
@@ -1,6 +1,7 @@
 %add radius to everything
 %add sveral obstacle and goals
 %implement ODE
+
 clc;
 clear all;
 close all;
@@ -11,48 +12,57 @@ y = 0;
 z = 0;
 d = 0.5; %distance to change from conic to parabolic well
 
-qgoal= [10,10,10]; %in earth frame
-qp = [x,y,z]; %should import form modell, current position
-
-
+%qgoal= [10,10,10]; %in earth frame
+%qp = [x,y,z]; %should import form model, current position
 %Printing initial position and goal
-scatter3(qp(1),qp(2), qp(3))
-hold on
-scatter3(qgoal(1),qgoal(2), qgoal(3))
-hold on
+%scatter3(qp(1),qp(2), qp(3))
+%hold on
+%scatter3(qgoal(1),qgoal(2), qgoal(3))
+%hold on
 
+%Constants for all the vehicles
 xi = 1; %scale factor for attractive potential
 eta = 0.5; %scale factor for repulsive potential
 p0 = 2; %radius of sphere of influence for repulsive potential
 
+%Generating random # of drones (initial pos+goal) between min and max
+min = 3;
+max = 8;
+[totaldrones, drones, goaldrones, colordrones] = uavgenerator (min, max);
+
 %Generating random # of obstacles between min and max
-min = 100;
-max= 300;
-[totalobst,obstpos] = obstaclegenerator(min,max);
+%The obstacles can't be in the same position as the drones' initial
+%position or their goals
+%Currently this is solved because obstacles can only appear between 1 and 9
+%and drones/goals between 0 and 1, and 9 and 10 respectively
+min = 50;
+max= 100;
+[totalobst,obstpos] = obstaclegenerator(min, max, drones, goaldrones);
 
+finished = 0;
+while(finished ~= totaldrones)
+    for i=1:totaldrones
+        rhogoal = norm(drones(i,:)-goaldrones(i,:));
+        if(rhogoal < 0.1)
+            finished = finished + 1;
+        else
+            %Attractive force and potential calculation
+            [Uatt, Fatt] = attractive(d, xi, drones(i,:), goaldrones(i,:), rhogoal);
 
-Fdir = [0,0,0]; %Force matrix with all force vectors on each time step
-v0 = 0; %Initial velocity
-rhogoal = norm(qp-qgoal); %Distance to goal
-while rhogoal > 0.1
+            %Repulsive force and potential calculation
+            [Ureptotstatic, Freptotstatic] = repulsive(p0, eta, drones(i,:), totalobst, obstpos);
 
-    %Attractive force and potential calculation
-    [Uatt, Fatt] = attractive(d, xi, qp, qgoal, rhogoal);
-    
-    %Repulsive force and potential calculation
-    [Ureptotstatic, Freptotstatic] = repulsive(p0, eta, qp, totalobst, obstpos);
-    
-    %Total force and potential -> attractive + repulsive
-    U = Uatt+Ureptotstatic;
-    F = Fatt+Freptotstatic;
-    
-    %Moving vehicle according to the force vector obtained
-    Fdir = [Fdir; F/norm(F)];
-    vvec = F.*tstep %+ v0;
-    qp = F*tstep + qp;
-    scatter3(qp(1),qp(2),qp(3), 3,'b')
-    hold on
-    rhogoal = norm(qp-qgoal) %Updating distance to goal
-    pause(0.01)
-    %v0 = norm(vvec)
+            %Total force and potential -> attractive + repulsive
+            U = Uatt+Ureptotstatic;
+            F = Fatt+Freptotstatic;
+
+            %Moving vehicle according to the force vector obtained
+            vvec = F.*tstep;
+            drones(i,:) = F*tstep + drones(i,:);
+            
+            scatter3(drones(i,1),drones(i,2),drones(i,3),3,colordrones(i,:))
+            hold on
+        end
+    end
+    pause(0.001)
 end
