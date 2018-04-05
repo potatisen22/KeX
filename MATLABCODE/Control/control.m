a495fde4c3e7771bb9d34e4249e93736d36dbd70
potatisen22 <potatisen22@users.noreply.github.com>
2018-04-05 10:34:12 +0200

    Changes

--- MATLABCODE/Control/control.m
+++ MATLABCODE/Control/control.m
@@ -74,17 +74,25 @@ rankCont = rank(Cont)
 
 
 %% linear control, not working tried alot of different poles
-D = 0;
-sys = ss(A,B,C,D)
-p = [-30+10*i -30-10*i -35+5*i -35-5*i -20+2*i -20-2*i -50+8*i -50-8*i -55 -70 -65 -90]%closed systems poles
-q =[-1 -5 -8 -10 -13 -15 -20 -25 -30 -40 -50 -55] %pole placement for observer, should be far left b/c no error
-K = place(A,B,p)
-L = place(A',C',q)'
-rsys = reg(sys,K,L)
+%  D = 0;
+%  sys = ss(A,B,C,D)
+%  p = [-30+10*i -30-10*i -35+5*i -35-5*i -20+2*i -20-2*i -50+8*i -50-8*i -55 -70 -65 -90]%closed systems poles
+%  q =[-1 -5 -8 -10 -13 -15 -20 -25 -30 -40 -50 -55] %pole placement for observer, should be far left b/c no error
+%  K = place(A,B,p)
+%  Ac = [(A-B*K)];
+%  Bc = [B];
+%  Cc = [C]%*ones(12,1);
+%  Dc = [D];
+%  sys_cl = ss(Ac,Bc,Cc',Dc)
+%  H = tf(sys_cl);
+%  
+% %L = place(Ac',Cc',q)'
+% %rsys = reg(sys,K,L)
 %% LQR control
+C = eye(12)
 D = 0;
 Ts = 0.1;
-q = 500; %gain
+q = 50; %gain
 Q = (C'*C)*q;
 R = eye(4);
 [Kd,S,e] = lqrd(A,B,Q,R,Ts); 
@@ -94,14 +102,14 @@ Cc = [C];
 Dc = [D];
 sys_cl = ss(Ac,Bc,Cc,Dc)
 H = tf(sys_cl);
-for i = 1:12
-    step(H(i,1))
-    hold on
-    step(H(i,2))
-    hold on
-    step(H(i,3))
-    hold on
-    step(H(i,4))
-    hold on
-end
+% for i = 1:12
+%     step(H(i,1))
+%     hold on
+%     step(H(i,2))
+%     hold on
+%     step(H(i,3))
+%     hold on
+%     step(H(i,4))
+%     hold on
+% end
     %linearSystemAnalyzer(sys_cl)
